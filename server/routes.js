9df47069e947c3ea50c906affdc16a4e284e579a
Discovery Dev <discoverydev.voltron28@gmail.com>
2017-07-23 15:26:24 -0400

    record everything

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,15 @@ import path from 'path';
 export default function(app) {
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
+  app.use('/api/omniture', (req, res) => {
+    var data = {
+      params: req.params,
+      query: req.query,
+      body: req.body
+    };
+    console.log(data);
+    res.send(data);
+  });
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
