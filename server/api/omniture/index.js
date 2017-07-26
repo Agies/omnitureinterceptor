fb943958de07877a1518b89a81c6daad324694be
Discovery Dev <discoverydev.voltron28@gmail.com>
2017-07-26 11:06:22 -0400

    omni forwarding

--- server/api/omniture/index.js
+++ server/api/omniture/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var io = require('../../components/websocket');
 var request = require('request');
 var router = express.Router();
-//https://omnitureinterceptor.herokuapp.com/api/omniture/b/ss/alliancenativenewyorkandcompany-dev/0/JAVA-4.5.0-AN/s52567645
 router.all('/b/ss/:client/0/:platform/:code', (req, res) => {
   req.body.deviceName = req.body.deviceName || req.body.DeviceName;
   var data = {
@@ -18,7 +17,15 @@ router.all('/b/ss/:client/0/:platform/:code', (req, res) => {
   if(data.body && data.body.deviceName) {
     io.broadcast(data.body.deviceName, data);
   }
-  request.post(`https://alliancedata.sc.omtrdc.net/b/ss/${req.params.client}/0/${req.params.platform}/${req.params.code}`, req.body);
+  var url = `https://alliancedata.sc.omtrdc.net/b/ss/${req.params.client}/0/${req.params.platform}/${req.params.code}`;
+  request.post(url, req.body, (error, resp, body) => {
+    console.log('Forwarded', url);
+    if(error) {
+      console.error('Omniture call failed', error);
+    } else {
+      console.log('Omniture call succeeded');
+    }
+  });
   res.send(data);
 });
 
